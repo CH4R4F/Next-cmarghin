2466c724026213d4996ba6498e72f92b55f91728
charaf marghin <developermarghin@gmail.com>
2022-11-10 02:57:46 +0100

    add component with assets and layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "@fontsource/lato";
+import "@fontsource/poppins/700.css";
+import "@fontsource/poppins/300.css";
+import "../assets/styles/globals.css";
+import Layout from "../components/common/Layout";
+import App from "next/app";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
